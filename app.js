26e8d318496732b839213973e07f144607fe1e84
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-05-12 20:15:47 +0300

    feature(core): Adding a cookie notification bar to the web site

--- app.js
+++ app.js
@@ -6,24 +6,26 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
 
-/////////////////////////////
 // Cookie Notification Issues
 
 const cookieMessage = document.createElement('div');
 
 cookieMessage.classList.add('cookie-message');
+cookieMessage.style.backgroundColor = '#37383D';
+cookieMessage.style.width = '104.25%';
 cookieMessage.innerHTML = `
-  We use cookies for improved functionality and analytics. 
-  <button class="btn btn--close-cookie">Got it!</button>
+We use cookies for improved functionality and analytics. 
+<button class="btn btn--close-cookie">Got it!</button>
 `;
 
-header.prepend(cookieMessage);
+header.append(cookieMessage);
+cookieMessage.style.height =
+  parseFloat(getComputedStyle(cookieMessage).height, 10) * 1.5 + 'px';
 
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', () => cookieMessage.remove());
 
-///////////////////
 // Modal Operations
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
